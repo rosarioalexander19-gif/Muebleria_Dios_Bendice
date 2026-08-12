3ada90ee2ad442ad9da1ea47de75ffdb04efad42
mariaisabelreyez26-glitch <mariaisabelreyez26@gmail.com>
2026-08-11 20:29:03 -0400

    Update usuario.py

--- model/usuario.py
+++ model/usuario.py
@@ -1,1 +1,12 @@
 
+class Usuario:
+    def __init__(self, id_usuario=None, nombre="", usuario="", password=""):
+        self.id_usuario = id_usuario
+        self.nombre = nombre
+        self.usuario = usuario
+        self.password = password
+
+    def __str__(self):
+        return f"{self.id_usuario} - {self.nombre} - {self.usuario}"
+
+
