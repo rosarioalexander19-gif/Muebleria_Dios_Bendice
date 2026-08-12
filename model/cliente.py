899ebdc48795e0ecf05edfb8c45823bcfb98d723
mariaisabelreyez26-glitch <mariaisabelreyez26@gmail.com>
2026-08-11 20:35:23 -0400

    Update cliente.py

--- model/cliente.py
+++ model/cliente.py
@@ -1,1 +1,26 @@
 
+class Cliente:
+    def __init__(
+        self,
+        id_cliente=None,
+        nombre_completo="",
+        cedula="",
+        telefono="",
+        direccion=""
+    ):
+        self.id_cliente = id_cliente
+        self.nombre_completo = nombre_completo
+        self.cedula = cedula
+        self.telefono = telefono
+        self.direccion = direccion
+
+    def __str__(self):
+        return (
+            f"{self.id_cliente} - "
+            f"{self.nombre_completo} - "
+            f"{self.cedula} - "
+            f"{self.telefono} - "
+            f"{self.direccion}"
+        )
+
+
