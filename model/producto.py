a1e818bbd8d8e207c42f0db2b87585815370a05a
Jennifer Flores <castillojennifer655@gmail.com>
2026-08-13 18:38:37 -0400

    Update producto.py

--- model/producto.py
+++ model/producto.py
@@ -1,1 +1,10 @@
+class Producto:
+    def __init__(self, id_producto=None, nombre="", categoria="", precio=0, stock=0):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.categoria = categoria
+        self.precio = precio
+        self.stock = stock
 
+    def __str__(self):
+        return f"{self.nombre} - RD${self.precio} - Stock: {self.stock}"
