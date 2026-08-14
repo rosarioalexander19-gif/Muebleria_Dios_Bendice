44364ab86e850251ed4287e2f6116b2ad5e1ebef
claritzabarry018 <claritzabarry018@gmail.com>
2026-08-14 16:16:18 -0400

    Refactor user creation and login methods
    Refactor crear_usuario and iniciar_sesion methods to use more descriptive parameter names and include role as an optional parameter.

--- service/usuario_service.py
+++ service/usuario_service.py
@@ -1,25 +1,24 @@
+from model.usuario import Usuario
+
 
 class UsuarioService:
 
     def __init__(self, usuario_repository):
         self.usuario_repository = usuario_repository
 
-    def crear_usuario(self, nombre, usuario, password):
+    def crear_usuario(self, nombre_usuario, contrasena, rol="Usuario"):
 
-        if not nombre:
-            raise ValueError("El nombre es obligatorio.")
+        if not nombre_usuario:
+            raise ValueError("El nombre de usuario es obligatorio.")
 
-        if not usuario:
-            raise ValueError("El usuario es obligatorio.")
-
-        if not password:
+        if not contrasena:
             raise ValueError("La contraseña es obligatoria.")
 
         nuevo_usuario = self.usuario_repository.crear(
             Usuario(
-                nombre=nombre,
-                usuario=usuario,
-                password=password
+                nombre_usuario=nombre_usuario,
+                contrasena=contrasena,
+                rol=rol
             )
         )
 
@@ -28,20 +27,22 @@ class UsuarioService:
     def obtener_usuarios(self):
         return self.usuario_repository.obtener_todos()
 
-    def iniciar_sesion(self, usuario, password):
+    def iniciar_sesion(self, nombre_usuario, contrasena):
 
         usuario_encontrado = (
-            self.usuario_repository.buscar_por_usuario(usuario)
+            self.usuario_repository.buscar_por_usuario(nombre_usuario)
         )
 
         if usuario_encontrado is None:
             return False
 
-        return usuario_encontrado.password == password
+        if usuario_encontrado.estado != "Activo":
+            return False
+
+        return usuario_encontrado.contrasena == contrasena
 
     def actualizar_usuario(self, usuario):
         self.usuario_repository.actualizar(usuario)
 
     def eliminar_usuario(self, id_usuario):
         self.usuario_repository.eliminar(id_usuario)
-
